a5384fa8490d9bb4d23e7433da9cdd8690e10ff1
Vladimir Sirel <exit551@ya.ru>
2022-01-09 13:30:35 +0300

    created new project - vkswiftUI

---
+++ vkswiftUI/vkswiftUI/vkswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkswiftUIApp.swift
+//  vkswiftUI
+//
+//  Created by Home on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct vkswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
